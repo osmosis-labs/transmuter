eeaf5af5de2106a7d9ba5bc1679685d74bc02824
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2023-06-06 15:44:27 +0700

    remove share state

--- contracts/transmuter/src/contract.rs
+++ contracts/transmuter/src/contract.rs
@@ -106,10 +106,6 @@ impl Transmuter<'_> {
 
         let new_shares = Shares::calc_shares(&info.funds)?;
 
-        // update shares
-        self.shares
-            .add_share(deps.storage, &info.sender, new_shares)?;
-
         // update pool
         self.pool
             .update(deps.storage, |mut pool| -> Result<_, ContractError> {
@@ -142,7 +138,7 @@ impl Transmuter<'_> {
         let (deps, env, info) = ctx;
 
         // check if sender's shares is enough
-        let sender_shares = self.shares.get_share(deps.as_ref().storage, &info.sender)?;
+        let sender_shares = self.shares.get_share(deps.as_ref(), &info.sender)?;
 
         let required_shares = Shares::calc_shares(&tokens_out)?;
 
@@ -153,10 +149,6 @@ impl Transmuter<'_> {
                 available: sender_shares
             }
         );
-
-        // update shares
-        self.shares
-            .sub_share(deps.storage, &info.sender, required_shares)?;
 
         // exit pool
         self.pool
@@ -339,7 +331,7 @@ impl Transmuter<'_> {
         Ok(GetSharesResponse {
             shares: self
                 .shares
-                .get_share(deps.storage, &deps.api.addr_validate(&address)?)?,
+                .get_share(deps, &deps.api.addr_validate(&address)?)?,
         })
     }
 
@@ -376,7 +368,7 @@ impl Transmuter<'_> {
         ctx: (Deps, Env),
     ) -> Result<GetTotalSharesResponse, ContractError> {
         let (deps, _env) = ctx;
-        let total_shares = self.shares.get_total_shares(deps.storage)?;
+        let total_shares = self.shares.get_total_shares(deps)?;
         Ok(GetTotalSharesResponse { total_shares })
     }
 
