0ead0702391d18764e7b29d9a2a6652214e4d987
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2023-08-15 15:53:18 +0700

    make codecov runs

--- contracts/transmuter/src/test/mod.rs
+++ contracts/transmuter/src/test/mod.rs
@@ -1,3 +1,7 @@
+// Ignore integration tests for code coverage since there will be problems with dynamic linking libosmosistesttube
+// and also, tarpaulin will not be able read coverage out of wasm binary anyway
+#![cfg(not(tarpaulin))]
+
 mod cases;
 mod modules;
 mod test_env;
