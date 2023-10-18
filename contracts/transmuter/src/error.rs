0a77a6b4f406cec3fc663fde1363797b405840a3
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2023-10-18 15:45:39 +0700

    implement add new assets function

--- contracts/transmuter/src/error.rs
+++ contracts/transmuter/src/error.rs
@@ -80,8 +80,11 @@ pub enum ContractError {
     #[error("The pool is currently inactive")]
     InactivePool {},
 
-    #[error("YUnexpected denom: expected: {expected}, actual: {actual}")]
+    #[error("Unexpected denom: expected: {expected}, actual: {actual}")]
     UnexpectedDenom { expected: String, actual: String },
+
+    #[error("Duplicated pool asset denom: {denom}")]
+    DuplicatedPoolAssetDenom { denom: String },
 
     #[error("Unauthorized")]
     Unauthorized {},
