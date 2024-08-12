630ead2fcba4c66ed616dc073cfa64998977406c
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2024-08-12 13:32:59 +0700

    extract transmuter math as package

---
+++ packages/transmuter_math/src/errors.rs
@@ -0,0 +1,24 @@
+use cosmwasm_std::{CheckedFromRatioError, DivideByZeroError, OverflowError};
+use thiserror;
+
+#[derive(thiserror::Error, Debug, PartialEq)]
+pub enum TransmuterMathError {
+    /// Time invariant error, this should never happen
+    #[error("Time must be monotonically increasing")]
+    NonMonotonicTime,
+
+    #[error("Moving average is undefined due to zero elapsed time since limiter started tracking")]
+    UndefinedMovingAverage,
+
+    #[error("Missing data points to calculate moving average")]
+    MissingDataPoints,
+
+    #[error("{0}")]
+    OverflowError(#[from] OverflowError),
+
+    #[error("{0}")]
+    DivideByZeroError(#[from] DivideByZeroError),
+
+    #[error("{0}")]
+    CheckedFromRatioError(#[from] CheckedFromRatioError),
+}
