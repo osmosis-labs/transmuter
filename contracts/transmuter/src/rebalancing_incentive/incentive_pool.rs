7f2bc458f5a33220782ae800d19cbb115b8c45a6
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2024-10-30 10:08:54 +0700

    rename coin in test

--- contracts/transmuter/src/rebalancing_incentive/incentive_pool.rs
+++ contracts/transmuter/src/rebalancing_incentive/incentive_pool.rs
@@ -70,23 +70,23 @@ mod tests {
     fn test_collect() {
         let mut pool = IncentivePool::new();
 
-        // Collect 100 uatom
-        pool.collect(coin(100, "uatom")).unwrap();
-        assert_eq!(pool.pool.get("uatom"), Some(&Uint128::new(100)));
+        // Collect 100 uusdt
+        pool.collect(coin(100, "uusdt")).unwrap();
+        assert_eq!(pool.pool.get("uusdt"), Some(&Uint128::new(100)));
 
-        // Collect another 50 uatom
-        pool.collect(coin(50, "uatom")).unwrap();
-        assert_eq!(pool.pool.get("uatom"), Some(&Uint128::new(150)));
+        // Collect another 50 uusdt
+        pool.collect(coin(50, "uusdt")).unwrap();
+        assert_eq!(pool.pool.get("uusdt"), Some(&Uint128::new(150)));
 
-        // Collect 200 uluna
-        pool.collect(coin(200, "uluna")).unwrap();
-        assert_eq!(pool.pool.get("uluna"), Some(&Uint128::new(200)));
+        // Collect 200 uusdc
+        pool.collect(coin(200, "uusdc")).unwrap();
+        assert_eq!(pool.pool.get("uusdc"), Some(&Uint128::new(200)));
 
         assert_eq!(
             pool.pool,
             BTreeMap::from([
-                ("uatom".to_string(), Uint128::new(150)),
-                ("uluna".to_string(), Uint128::new(200)),
+                ("uusdt".to_string(), Uint128::new(150)),
+                ("uusdc".to_string(), Uint128::new(200)),
             ])
         );
     }
@@ -96,28 +96,28 @@ mod tests {
         let mut pool = IncentivePool::new();
 
         // Collect some tokens first
-        pool.collect(coin(100, "uatom")).unwrap();
-        pool.collect(coin(200, "uluna")).unwrap();
+        pool.collect(coin(100, "uusdt")).unwrap();
+        pool.collect(coin(200, "uusdc")).unwrap();
 
-        // Deduct 50 uatom
-        pool.deduct(vec![coin(50, "uatom")]).unwrap();
-        assert_eq!(pool.pool.get("uatom"), Some(&Uint128::new(50)));
+        // Deduct 50 uusdt
+        pool.deduct(vec![coin(50, "uusdt")]).unwrap();
+        assert_eq!(pool.pool.get("uusdt"), Some(&Uint128::new(50)));
 
-        // Deduct another 50 uatom, should be zero now
-        pool.deduct(vec![coin(50, "uatom")]).unwrap();
-        assert_eq!(pool.pool.get("uatom"), None);
+        // Deduct another 50 uusdt, should be zero now
+        pool.deduct(vec![coin(50, "uusdt")]).unwrap();
+        assert_eq!(pool.pool.get("uusdt"), None);
 
-        // Deduct 100 uluna
-        pool.deduct(vec![coin(100, "uluna")]).unwrap();
-        assert_eq!(pool.pool.get("uluna"), Some(&Uint128::new(100)));
+        // Deduct 100 uusdc
+        pool.deduct(vec![coin(100, "uusdc")]).unwrap();
+        assert_eq!(pool.pool.get("uusdc"), Some(&Uint128::new(100)));
 
         // Try to deduct more than available, should return an error
-        let err = pool.deduct(vec![coin(150, "uluna")]).unwrap_err();
+        let err = pool.deduct(vec![coin(150, "uusdc")]).unwrap_err();
         assert_eq!(
             err,
             ContractError::UnableToDeductFromIncentivePool {
-                required: coin(150, "uluna"),
-                available: coin(100, "uluna"),
+                required: coin(150, "uusdc"),
+                available: coin(100, "uusdc"),
             }
         );
     }
