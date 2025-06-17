f00b10cd5cd5074160a6a3dfddb7a884a8a84496
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2025-06-17 12:49:50 +0700

    add round adj test

--- packages/transmuter_math/src/rebalancing/mod.rs
+++ packages/transmuter_math/src/rebalancing/mod.rs
@@ -10,6 +10,29 @@ use cosmwasm_std::{Decimal, Int256, SignedDecimal256, StdError, StdResult, Uint1
 
 const DECIMAL_FRACTIONAL: Int256 = Int256::from_i128(1_000_000_000_000_000_000);
 
+/// Round a SignedDecimal256 to Int256 with appropriate rounding behavior:
+/// - For positive values: round down (give less incentive)
+/// - For negative values: round up (take more fee)
+fn round_adjustment(adjustment: SignedDecimal256) -> Result<Int256, Error> {
+    if adjustment > SignedDecimal256::zero() {
+        // For positive adjustments (incentives), round down to give less
+        Ok(adjustment.atomics().checked_div(DECIMAL_FRACTIONAL)?)
+    } else {
+        // For negative adjustments (fees), round up to take more
+        let atomics = adjustment.atomics();
+        let truncated = atomics.checked_div(DECIMAL_FRACTIONAL)?;
+        let truncated_with_zeros = truncated.checked_mul(DECIMAL_FRACTIONAL)?;
+
+        // If there is a remainder, because this is a negative value, truncated will be greater than the actual value.
+        // So we need to subtract 1 to get the correct value.
+        if truncated_with_zeros > atomics {
+            Ok(truncated.checked_sub(Int256::from(1))?)
+        } else {
+            Ok(truncated)
+        }
+    }
+}
+
 /// Compute fee or incentive adjustment for a single asset's balance movement.
 ///
 /// This function calculates the incentive (if positive) or fee (if negative)
@@ -44,23 +67,25 @@ pub fn compute_adjustment_value(
         0,
     )?)?;
 
-    // Convert to Uint128 with appropriate rounding
-    if adjustment_value > SignedDecimal256::zero() {
-        // For positive adjustments (incentives), round down to give less
-        Ok(adjustment_value.atomics().checked_div(DECIMAL_FRACTIONAL)?)
-    } else {
-        // For negative adjustments (fees), round up to take more
-        let atomics = adjustment_value.atomics();
-        let truncated = atomics
-            .checked_div(DECIMAL_FRACTIONAL)?
-            .checked_mul(DECIMAL_FRACTIONAL)?;
+    round_adjustment(adjustment_value)
+}
 
-        // If there is a remainder, because this is a negative value, truncated will be greater than the actual value.
-        // So we need to subtract 1 to get the correct value.
-        if truncated > atomics {
-            Ok(truncated.checked_sub(Int256::from(1))?)
-        } else {
-            Ok(truncated)
-        }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(SignedDecimal256::percent(100), Int256::from(1))] // 1.0 -> 1
+    #[case(SignedDecimal256::percent(150), Int256::from(1))] // 1.5 -> 1
+    #[case(SignedDecimal256::percent(199), Int256::from(1))] // 1.99 -> 1
+    #[case(SignedDecimal256::percent(200), Int256::from(2))] // 2.0 -> 2
+    #[case(SignedDecimal256::percent(-100), Int256::from(-1))] // -1.0 -> -1
+    #[case(SignedDecimal256::percent(-150), Int256::from(-2))] // -1.5 -> -2
+    #[case(SignedDecimal256::percent(-199), Int256::from(-2))] // -1.99 -> -2
+    #[case(SignedDecimal256::percent(-200), Int256::from(-2))] // -2.0 -> -2
+    #[case(SignedDecimal256::zero(), Int256::zero())] // 0.0 -> 0
+    fn test_round_adjustment(#[case] input: SignedDecimal256, #[case] expected: Int256) {
+        assert_eq!(round_adjustment(input).unwrap(), expected);
     }
 }
